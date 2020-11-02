b99a3bb64b442e53df9e473984d74c29260e0017
磊 孙 <77540975@qq.com>
2020-11-02 15:44:13 +0800

    1st commit.

---
+++ APIGateway.Ocelot/Startup.cs
@@ -0,0 +1,55 @@
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+
+namespace APIGateway.Ocelot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication()
+            .AddJwtBearer("defaultJwtAuth", jbo =>
+            {
+                jbo.RequireHttpsMetadata = false;
+                jbo.SaveToken = true;
+                jbo.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["DefaultJwtAuth:Secret"])),
+                    ValidIssuer = Configuration["DefaultJwtAuth:Issuer"],
+                    ValidAudience = Configuration["DefaultJwtAuth:Audience"],
+
+                };
+            });
+
+            services.AddOcelot()
+            .AddConsul()
+            .AddCacheManager(x => x.WithDictionaryHandle());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseOcelot().Wait();
+        }
+    }
+}
