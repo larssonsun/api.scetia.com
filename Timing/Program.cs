9eb23f73eba76bd87a231ea3ea9dfd88306e1863
磊 孙 <77540975@qq.com>
2020-11-12 10:14:43 +0800

    Added timing tools for iis kerstel fuking bug for ocelot.

---
+++ Timing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using RestSharp;
+
+namespace Timing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Client = new RestClient("http://api.scetia.com");
+            var request = new RestRequest("/obsolete.df/user", Method.GET);
+            Console.WriteLine("started.");
+            while(true)
+            {
+                Client.Execute(request);
+                Thread.Sleep(20000);
+            }
+        }
+    }
+}
