aa66a3a414046df2e8d605695cf5a8c89032a7ca
磊 孙 <77540975@qq.com>
2020-11-13 15:15:13 +0800

    .

--- APIGateway.Ocelot.Empty/Program.cs
+++ APIGateway.Ocelot.Empty/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.HttpSys;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -25,7 +26,15 @@ namespace APIGateway.Ocelot.Empty
 
              .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseHttpSys(options =>
+                    {
+                        // The following options are set to default values.
+                        options.Authentication.Schemes = AuthenticationSchemes.None;
+                        options.Authentication.AllowAnonymous = true;
+                        options.MaxConnections = null;
+                        options.MaxRequestBodySize = 30000000;
+                        options.UrlPrefixes.Add("http://api.scetia.com:80");
+                    });
                 });
     }
 }
